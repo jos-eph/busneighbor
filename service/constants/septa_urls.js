1c2f59e9c2bab5859809ed000d674efe1e97ba3d
Joe Mirarchi <joe@dydx.org>
2024-09-21 23:55:53 -0400

    URL stubs added in - need calls and populators

--- service/constants/septa_urls.js
+++ service/constants/septa_urls.js
@@ -3,4 +3,13 @@ const SEPTA_BASE = 'https://www3.septa.org/api'
 const LOCATION_URL = SEPTA_BASE + '/TransitView/index.php?'
 const ROUTE_ALERTS = SEPTA_BASE + '/Alerts/get_alert_data.php?'
 
+
+// https://www3.septa.org/api/v2/trips/?route_id=45
+// https://www3.septa.org/api/v2/alerts/?route_id=45
+
+const V2 = SEPTA_BASE + "/v2";
+const LOCATION_URL_V2 = V2 + "/trips/";
+const ALERTS_URL_V2 = V2 + "/alerts/";
+
+
 export {PROXY, LOCATION_URL, ROUTE_ALERTS}
