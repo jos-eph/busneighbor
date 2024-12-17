01360859884ef0bb55fb82079c8dd79f04503b78
jos-eph <85128396+jos-eph@users.noreply.github.com>
2024-12-17 13:28:40 +0000

    Not quite working right

--- service/tabledisplay/tabledisplayservice.js
+++ service/tabledisplay/tabledisplayservice.js
@@ -1,3 +1,13 @@
 import {  INFO_BOX, INFO_TEXT, ROUTE_GROUP, SINGLE_ROUTE_STATUS, VEHICLE_POSITIONS, BUS_NUMBER,
     INDIVIDUAL_DIRECTION, DIRECTION_HEADER, STATUS_HOLDER, STREET_POSITION, ALERT_MESSAGE } from './tabledisplayconstants.js'
 
+function createDivOfClasses(classes, textContent) {
+    let newElement = document.createElement('div');
+    for (let cls of classes) {
+        newElement.classList.add(cls);
+    }
+    newElement.innerText = textContent;
+    return newElement;
+}
+
+export { createDivOfClasses };
--- tabledisplaydemo.js
+++ tabledisplaydemo.js
@@ -1,1 +1,16 @@
+import {  INFO_BOX, INFO_TEXT, ROUTE_GROUP, SINGLE_ROUTE_STATUS, VEHICLE_POSITIONS, BUS_NUMBER,
+    INDIVIDUAL_DIRECTION, DIRECTION_HEADER, STATUS_HOLDER, STREET_POSITION, ALERT_MESSAGE } 
+    from './service/tabledisplay/tabledisplayconstants.js'
+
+import { createDivOfClasses } from './service/tabledisplay/tabledisplayservice.js'
+
 alert("Table demo script started!")
+
+function createAndAppend(classes, text) {
+    const newElement = createDivOfClasses(classes, text);
+    document.body.append(newElement);
+}
+
+createAndAppend([INFO_BOX, INFO_TEXT], "Ÿoto");
+createAndAppend([[INFO_BOX, STREET_POSITION], "yellow"]);
+createAndAppend([INFO_BOX, INFO_TEXT, ALERT_MESSAGE], "Hello alert!")
