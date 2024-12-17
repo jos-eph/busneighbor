b67483d3d0431a669268af40ffebb866f579b92f
jos-eph <85128396+jos-eph@users.noreply.github.com>
2024-12-17 12:56:06 +0000

    small edits

--- service/tabledisplay/tabledisplayservice.js
+++ service/tabledisplay/tabledisplayservice.js
@@ -1,14 +1,3 @@
-const INFO_BOX = "infoBox";
-const INFO_TEXT = "infoText";
+import {  INFO_BOX, INFO_TEXT, ROUTE_GROUP, SINGLE_ROUTE_STATUS, VEHICLE_POSITIONS, BUS_NUMBER,
+    INDIVIDUAL_DIRECTION, DIRECTION_HEADER, STATUS_HOLDER, STREET_POSITION, ALERT_MESSAGE } from './tabledisplayconstants.js'
 
-const ROUTE_GROUP = "routeInfoDisplays";
-    const SINGLE_ROUTE_STATUS = "statusLineWithAlertMessage";
-        const VEHICLE_POSITIONS = "statusLine";
-            const BUS_NUMBER = "busNumber";
-            const INDIVIDUAL_DIRECTION = "singleDirectionInfo";
-                const DIRECTION_HEADER= "directionHeader";
-
-                const STATUS_HOLDER = "statusHolder";
-                    const STREET_POSITION = "streetPosition";
-
-        const ALERT_MESSAGE = "alertMessage";
