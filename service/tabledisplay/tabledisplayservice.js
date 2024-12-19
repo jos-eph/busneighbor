f811df8ca250dec57a9976049b98b36ac9e75166
jos-eph <85128396+jos-eph@users.noreply.github.com>
2024-12-19 13:59:26 +0000

    updated CSS

--- service/tabledisplay/tabledisplayservice.js
+++ service/tabledisplay/tabledisplayservice.js
@@ -11,6 +11,7 @@ function createDivOfClasses(classes, textContent) {
     for (let cls of classes) {
         newElement.classList.add(cls);
     }
+    newElement.classList.add("showMe")
     if (textContent !== undefined) {
         newElement.innerText = textContent;
     }
