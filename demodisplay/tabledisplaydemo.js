37f2d17205cd8f60567b897398c0e47fb07e83ef
Joe Mirarchi <joe@dydx.org>
2024-12-19 10:20:32 -0500

    move horizontal demo to demo folder

--- demodisplay/tabledisplaydemo.js
+++ demodisplay/tabledisplaydemo.js
@@ -1,9 +1,9 @@
 import {  INFO_BOX, INFO_TEXT, ROUTE_GROUP, SINGLE_ROUTE_STATUS, VEHICLE_POSITIONS, BUS_NUMBER,
     INDIVIDUAL_DIRECTION, DIRECTION_HEADER, STATUS_HOLDER, STREET_POSITION, ALERT_MESSAGE, STATUS_LINE_MESSAGE_GROUPS } 
-    from './service/tabledisplay/tabledisplayconstants.js'
+    from '../service/tabledisplay/tabledisplayconstants.js'
 
-import { createDivOfClasses, createStatusLineWithAlertMessage } from './service/tabledisplay/tabledisplayservice.js'
-import { DirectionLocations } from './model/directionLocations.js'
+import { createDivOfClasses, createStatusLineWithAlertMessage } from '../service/tabledisplay/tabledisplayservice.js'
+import { DirectionLocations } from '../model/directionLocations.js'
 
 // alert("Table demo script started!")
 
