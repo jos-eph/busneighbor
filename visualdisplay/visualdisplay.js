e0ccba113f48accaa9e79b709ae2ed3d6c92ad82
Joe Mirarchi <joe@dydx.org>
2025-01-17 08:46:44 -0500

    improved loading speed

--- visualdisplay/visualdisplay.js
+++ visualdisplay/visualdisplay.js
@@ -2,30 +2,37 @@ import { Store } from '../flowcontrol/store.js';
 import { ManagedMap } from "../service/leaflet/managedMap.js";
 import { getPushpins } from '../service/displayers/pushpin_displayer.js';
 
-console.log("Loading demo map painting...");
-
-// Define routes
-const routes = ["45", "33", "38", "29", "47", "4", "40"]
-
-const mapElement = document.getElementById("simplemap");
-const map = new ManagedMap(mapElement, "..");
-
-const store = new Store(routes);
-await store.initialize();
-
+// Main body functions
 function paintMap() {
   map.clearPushpins();
   map.populate(getPushpins(store), store.userLocation);
   console.log("userLocation for painting: ", store.userLocation);
 }
 
-// Main body functions
-async function cycleRefresh() {
+async function refreshAndPaint() {
   console.log("Requesting location refresh");
   await store.requestLocationsRefresh();
   store.indexLocations();
   paintMap();
 }
+
+async function cycleRefresh() {
+  await refreshAndPaint();
+}
+
+
+// Define routes
+const routes = ["45", "33", "38", "29", "47", "4", "40"]
+
+
+// Initialize map
+console.log("Loading demo map painting...");
+const mapElement = document.getElementById("simplemap");
+const map = new ManagedMap(mapElement, "..");
+
+const store = new Store(routes);
+await store.initialize();
+await refreshAndPaint();
 
 // Main loop
 setInterval(cycleRefresh, 5000);
