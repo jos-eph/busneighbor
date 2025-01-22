ab405e653e23a75a233717baa090172211b251e9
Joe Mirarchi <joe@dydx.org>
2025-01-22 14:26:57 -0500

    input control demo triggers

--- demodisplay/inputcontrol/demoinput.js
+++ demodisplay/inputcontrol/demoinput.js
@@ -1,1 +1,54 @@
+// Identify elements
+
+const ROUTE_SUBMISSION_FORM = "routeSubmissionForm";
+const ROUTE_SUBMISSION_INPUT = "busRouteInput";
+const KEYUP_EVENT = "keyup";
+const SUBMIT_EVENT = "submit";
+const ENTER_KEY = "Enter";
+
+const formElement = document.getElementById(ROUTE_SUBMISSION_FORM);
+const inputElement = document.getElementById(ROUTE_SUBMISSION_INPUT);
+
+// Create handlers
+function testEventOfType(event, type) {
+    if (!(event instanceof Event)) {
+        throw new TypeError("Not an event!");
+    }
+
+    if ((event.type) != type) {
+        throw new Error(`Type was ${event.type}, not ${type} as expected!`);
+    }
+    
+    return true;
+}
+
+/**
+ * React to keypresses
+ *
+ * @param {Event} keyUpEvent 
+ */
+function handleKeyPress(keyUpEvent) {
+    testEventOfType(keyUpEvent, KEYUP_EVENT);
+    if (keyUpEvent.key === ENTER_KEY) {
+        alert("Triggering fake submit!");
+    }
+}
+
+
+/**
+ * Description placeholder
+ *
+ * @param {*} submitEvent 
+ */
+function handleSubmitEvent(submitEvent) {
+    testEventOfType(submitEvent, SUBMIT_EVENT);
+    alert("Submit event triggered!");
+}
+
+
+
+// Register listeners
+inputElement.addEventListener(KEYUP_EVENT, handleKeyPress);
+formElement.addEventListener(SUBMIT_EVENT, handleSubmitEvent);
+
 console.log("Hello, world!");
