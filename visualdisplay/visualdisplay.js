842d30d0cf7d2eddcc4c9c21efe33a057b6ed2c3
Joe Mirarchi <joe@dydx.org>
2025-01-25 20:10:51 -0500

    beginning of input class

--- visualdisplay/visualdisplay.js
+++ visualdisplay/visualdisplay.js
@@ -1,6 +1,21 @@
 import { Store } from '../flowcontrol/store.js';
 import { ManagedMap } from "../service/leaflet/managedMap.js";
 import { getPushpins } from '../service/displayers/pushpin_displayer.js';
+import { RouteSelectionInput } from '../forminput/route_selection_input.js';
+import { TypedSet } from '../common/pickling/typedSet.js';
+
+// Set Defaults
+const DEFAULT_PERMITTED_ROUTES = new TypedSet(["45", "33", "38", "29", "47", "4", "40"]);
+
+// Identify Elements
+
+const formElement = document.getElementById("routeSubmissionForm");
+const inputElement = document.getElementById("busRouteInput");
+const submitElement = document.getElementById("submitButton");
+const checkboxesElement = document.getElementById("checkboxes");
+
+const routeSelectionInput = new RouteSelectionInput(DEFAULT_PERMITTED_ROUTES,
+  formElement, inputElement, submitElement, checkboxesElement);
 
 // Main body functions
 function paintMap() {
