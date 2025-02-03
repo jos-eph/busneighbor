be65e0849996f87eed13a9d0e8ce1e12d540c043
Joe Mirarchi <joe@dydx.org>
2025-02-03 10:15:05 -0500

    broken jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  moduleNameMapper: {
+    '^/(.*)$': '<rootDir>/$1', // Map root paths to the project root
+  },
   testMatch: ['**/*.test.mjs'],
   testPathIgnorePatterns: ['/node_modules/', '/.vscode/'],
   transform: { '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest' }
