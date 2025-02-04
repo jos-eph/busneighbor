f9115aefd9b9a967fbc5f050755d4c8e1ec4428d
Joe Mirarchi <joe@dydx.org>
2025-02-04 08:19:07 -0500

    update jest.config.mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,7 @@
 // jest.config.mjs
 export default {
   testMatch: [
-    "**/__tests__/**/*.test.mjs", // Matches your current structure
-    "**/?(*.)+(spec|test).mjs"   // Optional: For other potential test file names
+    "**/__tests__/**/*.test.mjs"
   ],
   moduleNameMapper: {
     '^/busneighbor/(.*)$': '<rootDir>/$1'
